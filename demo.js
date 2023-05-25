0f0acbe4ee95f282d824a1ca090362b27219f73b
wznmickey <first@wznmickey.com>
2023-05-25 15:05:08 +0800

    update ways to split url

--- demo.js
+++ demo.js
@@ -1,14 +1,16 @@
-function getCodeN(n, m) {
+function getCode() {
     try {
         temp = document.getElementsByClassName("course-list-table-row");
         data = [];
         for (let i = 0; i < temp.length; i++) {
             try {
                 url = temp[i].getElementsByClassName("course-list-course-title-column")[0].lastElementChild;
-                data.push(Number(url.href.slice(n, url.href.length - m)));
+                x = url.href.split("/");
+                y = x[x.length - 1]
+                data.push(Number(y));
             }
             catch (err) {
-
+                console.log(err);
             }
         }
         ans = { "courseIDs": data };
@@ -18,34 +20,4 @@ function getCodeN(n, m) {
 
     }
 }
-function getCodeURL(n, m) {
-    return getCodeN(n.length, m);
-}
-function getCodeSJTU_JI() {
-    return getCodeN(29, 0);
-}
-function getCodeSJTU() {
-    return getCodeURL("https://oc.sjtu.edu.cn/courses/", 0);
-}
-function getCode(n) {
-    before = {};
-    after = {};
-
-    before["SJTU"] = 31;
-    after["SJTU"] = 0;
-    before["SJTU_JI"] = 29;
-    after["SJTU_JI"] = 0;
-    //shotcut
-    before["JI"] = before["SJTU_JI"];
-    after["JI"] = after["SJTU_JI"];
-
-    //i18n
-    before["上海交通大学"] = before["SJTU"];
-    after["上海交通大学"] = after["SJTU"];
-    before["上交"] = before["SJTU"];
-    after["上交"] = after["SJTU"];
-    before["密院"] = before["SJTU_JI"];
-    after["密院"] = after["SJTU_JI"];
-
-    return getCodeN(before[n], after[n]);
-}
+getCode();
--- getCanvasCourseCode.js
+++ getCanvasCourseCode.js
@@ -1,45 +1,44 @@
-
-
-    // ==UserScript==
-    // @name        getCanvasCourseCode
-    // @namespace   getCanvasCourseCode
-    // @match       *://*canvas*/*
-    // @grant       none
-    // @version     1.0
-    // @author      wznmickey
-    // @description getCanvasCourseCode and show on the top of the page
-    // @license Apache License 2.0
-    // ==/UserScript==
-    function getCodeN(n, m) {
-        try {
-            temp = document.getElementsByClassName("course-list-table-row");
-            data = [];
-            for (let i = 0; i < temp.length; i++) {
-                try {
-                    url = temp[i].getElementsByClassName("course-list-course-title-column")[0].lastElementChild;
-                    data.push(Number(url.href.slice(n, url.href.length - m)));
-                }
-                catch (err) {
-     
-                }
+// ==UserScript==
+// @name:en         getCanvasCourseCode
+// @name:zh         获取Canvas课程代码
+// @namespace       getCanvasCourseCode
+// @match           *://*canvas*/course
+// @grant           none
+// @version         1.0
+// @author          wznmickey
+// @description:en  get Canvas Course Code and show on the top of the page
+// @description:zh  获取Canvas课程代码并展示在页面顶部
+// @license         Apache License 2.0
+// @language    
+// ==/UserScript==
+function getCode() {
+    try {
+        temp = document.getElementsByClassName("course-list-table-row");
+        data = [];
+        for (let i = 0; i < temp.length; i++) {
+            try {
+                url = temp[i].getElementsByClassName("course-list-course-title-column")[0].lastElementChild;
+                x = url.href.split("/");
+                y = x[x.length - 1]
+                data.push(Number(y));
             }
-            ans = { "courseIDs": data };
-            return JSON.stringify(ans);
+            catch (err) {
+                console.log(err);
+            }
         }
-        catch (err) {
-     
-        }
+        ans = { "courseIDs": data };
+        return JSON.stringify(ans);
     }
-    function getCodeURL(n, m) {
-        return getCodeN(n.length, m);
+    catch (err) {
+
     }
-    function showID(st)
-    {
-      x=document.createElement("div");
-      x.innerHTML=st;
-      y=document.getElementsByClassName("header-bar")[0].parentNode;
-      y.insertBefore(x,document.getElementsByClassName("header-bar")[0]);
-      console.log(st);
-    }
-    showID(getCodeURL(window.location.href+1),0);
+}
+function showID(st) {
+    x = document.createElement("div");
+    x.innerHTML = st;
+    y = document.getElementsByClassName("header-bar")[0].parentNode;
+    y.insertBefore(x, document.getElementsByClassName("header-bar")[0]);
+    console.log(st);
+}
+showID(getCode());
 
